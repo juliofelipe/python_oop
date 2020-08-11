bd24727c05de31774c7ac059f28ab9138d6cac4c
Julio Felipe <julio.felipe@casaldev.com.br>
2020-08-10 22:05:48 -0300

    herança

--- modelo.py
+++ modelo.py
@@ -1,16 +1,30 @@
-class Filme:
-  def __init__(self, nome, ano, duracao):
-    self.__nome = nome
+class Programa:
+   def __init__(self, nome, ano):
+    self.__nome = nome.title
     self.ano = ano
-    self.duracao = duracao
     self.__likes = 0
 
-  @property
-  def likes(self):
-    return self.__likes
-    
-  def dar_like(self):
-    self.likes += 1
+    @property
+    def likes(self):
+      return self.__likes
+
+    def dar_like(self):
+      self.likes += 1
+
+    @property
+    def nome(self):
+      return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+      self.__nome = novo_nome.title()
+
+
+class Filme:
+    def __init__(self, nome, ano):
+      self.__nome = nome.title
+      self.ano = ano
+      self.__likes = 0
 
 class Serie:
   def __init__(self, nome, ano, temporadas):
